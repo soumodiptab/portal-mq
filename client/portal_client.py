aa928745a4373b9c6a0ed4e041fffb1629497000
Soumodipta Bose <sbose2019@gmail.com>
2023-04-15 12:20:04 +0530

    start with the impl

---
+++ client/portal_client.py
@@ -0,0 +1,15 @@
+class PortalClient:
+    def __init__(self,portal_connection):
+        """_summary_
+
+        Args:
+            portal_connection ([array]): contains an array of portal connection objects
+            Example:
+            [{
+                "host": "localhost",
+                "port": 5000
+            },{
+                "host": "localhost",
+                "port": 5001
+            }]
+        """
